cebada5754d2562d0e16400d26302d03f0d21f01
Wallace <wbalaniu@my.centennialcollege.ca>
2020-06-15 14:43:29 -0400

    Initial CreateJS framework

---
+++ Scripts/core/game.js
@@ -0,0 +1,34 @@
+// Immediate Invoked Anonymous Function
+(function () {
+    // Global Game Variables
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
+    function Init() {
+        console.log("Initializing Start");
+        Start();
+    }
+    function Start() {
+        console.log("Starting Application...");
+        // Initialize CreateJS
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+    function Update() {
+        stage.Update();
+        // Movement here
+        helloLabel.scaleX += 0.001;
+        helloLabel.scaleY += 0.001;
+    }
+    function Main() {
+        console.log("Game Start");
+        helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel);
+    }
+    window.onload = Init;
+})();
+//# sourceMappingURL=game.js.map
---
+++ Scripts/core/game.ts
@@ -0,0 +1,44 @@
+// Immediate Invoked Anonymous Function
+
+(function() {
+
+    // Global Game Variables
+    let canvas = document.getElementById("canvas");
+    let stage:createjs.Stage;
+    let helloLabel:createjs.Text;
+
+    function Init() {
+        console.log("Initializing Start");
+        Start();
+    }
+
+    function Start() {
+        console.log("Starting Application...");
+
+        // Initialize CreateJS
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+
+    function Update() {
+        stage.Update();
+
+        // Movement here
+        helloLabel.scaleX += 0.001;
+        helloLabel.scaleY += 0.001;
+    }
+
+    function Main() {
+        console.log("Game Start");
+
+        helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+
+        stage.addChild(helloLabel);
+    }
+
+    window.onload = Init;
+})();
